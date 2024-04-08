85d235aafd5e44ba6ece93480d4261e4771bc629
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-04-08 17:38:33 +0530

    about redirected

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -43,7 +43,7 @@ const AboutSection = () => {
     });
   };
   return (
-    <section className="text-zinc-400">
+    <section id="about" className="text-zinc-400">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/about-image.png" width={500} height={900} alt="About Image"/>
         <div className="mt-4 md:mt-0 text-left flex-col h-full">
