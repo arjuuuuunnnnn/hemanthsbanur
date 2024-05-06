e0468cf246aa193486615cafb271ec54628a1059
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-05-06 21:32:02 +0530

    error page 2

--- src/app/error/page.jsx
+++ src/app/error/page.jsx
@@ -11,17 +11,17 @@ export const ErrorPage = () => {
   };
 
   return (
-    <section className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-emerald-950 via-emerald-900 to-lime-900">
+    <section className="flex flex-col items-center justify-center min-h-screen">
       <div className="max-w-md mx-auto p-6 bg-gray-800 rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold text-white mb-4">
-          Error cannot 'cat resume'
+        <h1 className="text-3xl font-bold text-zinc-300 mb-4">
+          <strong className="font-extrabold">Error!!</strong>cannot <strong className="font-extrabold text-emerald-800">cat resume</strong>
         </h1>
         <p className="text-gray-300 mb-6">
           There was an error while trying to access the resume. Please try again
           later.
         </p>
         <button
-          className="px-6 py-3 rounded-lg bg-emerald-600 hover:bg-emerald-700 text-white focus:outline-none"
+          className="px-6 py-3 w-full sm:w-fit rounded-lg mr-4 bg transparent border border-emerald-800 border-[3px] text-zinc-300"
           onClick={handleGoBack}
         >
           Go back
