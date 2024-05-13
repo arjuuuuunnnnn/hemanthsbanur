c1a5309ed35915cbe3565d26425f2fefe27dbb1e
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-05-13 15:11:09 +0530

    theme set

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -8,111 +8,156 @@ const TAB_DATA = [
     title: "devtools",
     id: "devtools",
     content: (
-     <>
+      <>
         <div className="flex flex-wrap">
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=azure" alt="Azure icon" />
+            <img src="https://skillicons.dev/icons?i=azure&theme=dark" alt="Azure icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=debian" alt="Debian icon" />
+            <img
+              src="https://skillicons.dev/icons?i=debian&theme=dark"
+              alt="Debian icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=docker" alt="Docker icon" />
+            <img
+              src="https://skillicons.dev/icons?i=docker&theme=dark"
+              alt="Docker icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=github" alt="GitHub icon" />
+            <img
+              src="https://skillicons.dev/icons?i=github&theme=dark"
+              alt="GitHub icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=git" alt="Git icon" />
+            <img src="https://skillicons.dev/icons?i=git&theme=dark" alt="Git icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=kali" alt="Kali Linux icon" />
+            <img
+              src="https://skillicons.dev/icons?i=kali&theme=dark"
+              alt="Kali Linux icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=linux" alt="Linux icon" />
+            <img src="https://skillicons.dev/icons?i=linux&theme=dark" alt="Linux icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=neovim" alt="Neovim icon" />
+            <img
+              src="https://skillicons.dev/icons?i=neovim&theme=dark"
+              alt="Neovim icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=unity" alt="Unity icon" />
+            <img src="https://skillicons.dev/icons?i=unity&theme=dark" alt="Unity icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=vim" alt="Vim icon" />
+            <img src="https://skillicons.dev/icons?i=vim&theme=dark" alt="Vim icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=vscode" alt="Visual Studio Code icon" />
+            <img
+              src="https://skillicons.dev/icons?i=vscode&theme=dark"
+              alt="Visual Studio Code icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=postman" alt="Postman icon" />
+            <img
+              src="https://skillicons.dev/icons?i=postman&theme=dark"
+              alt="Postman icon"
+            />
           </span>
         </div>
-      </> 
+      </>
     ),
   },
   {
     title: "webdev",
     id: "webdev",
     content: (
-     <>
+      <>
         <div className="flex flex-wrap">
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=html" alt="HTML icon" />
+            <img src="https://skillicons.dev/icons?i=html&theme=dark" alt="HTML icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=css" alt="CSS icon" />
+            <img src="https://skillicons.dev/icons?i=css&theme=dark" alt="CSS icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=js" alt="JavaScript icon" />
+            <img
+              src="https://skillicons.dev/icons?i=js&theme=dark"
+              alt="JavaScript icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=react" alt="React icon" />
+            <img src="https://skillicons.dev/icons?i=react&theme=dark" alt="React icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=nextjs" alt="Next.js icon" />
+            <img
+              src="https://skillicons.dev/icons?i=nextjs&theme=dark"
+              alt="Next.js icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=nodejs" alt="Node.js icon" />
+            <img
+              src="https://skillicons.dev/icons?i=nodejs&theme=dark"
+              alt="Node.js icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=tailwind" alt="Tailwind CSS icon" />
+            <img
+              src="https://skillicons.dev/icons?i=tailwind&theme=dark"
+              alt="Tailwind CSS icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=firebase" alt="Firebase icon" />
+            <img
+              src="https://skillicons.dev/icons?i=firebase&theme=dark"
+              alt="Firebase icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=flask" alt="Flask icon" />
+            <img src="https://skillicons.dev/icons?i=flask&theme=dark" alt="Flask icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=mongodb" alt="MongoDB icon" />
+            <img
+              src="https://skillicons.dev/icons?i=mongodb&theme=dark"
+              alt="MongoDB icon"
+            />
           </span>
         </div>
-      </> 
+      </>
     ),
   },
-{
+  {
     title: "langs",
     id: "langs",
     content: (
-         <>
+      <>
         <div className="flex flex-wrap">
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=c" alt="C icon" />
+            <img src="https://skillicons.dev/icons?i=c&theme=dark" alt="C icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=cpp" alt="C++ icon" />
+            <img src="https://skillicons.dev/icons?i=cpp&theme=dark" alt="C++ icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=py" alt="Python icon" />
+            <img src="https://skillicons.dev/icons?i=py&theme=dark" alt="Python icon" />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=tensorflow" alt="TensorFlow icon" />
+            <img
+              src="https://skillicons.dev/icons?i=tensorflow&theme=dark"
+              alt="TensorFlow icon"
+            />
           </span>
           <span className="mr-2 mb-2">
-            <img src="https://skillicons.dev/icons?i=flutter" alt="Flutter icon" />
+            <img
+              src="https://skillicons.dev/icons?i=flutter&theme=dark"
+              alt="Flutter icon"
+            />
           </span>
         </div>
-      </> 
+      </>
     ),
   },
 ];
@@ -129,7 +174,12 @@ const AboutSection = () => {
   return (
     <section id="about" className="text-zinc-400">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/About.png" width={500} height={900} alt="About Image"/>
+        <Image
+          src="/images/About.png"
+          width={500}
+          height={900}
+          alt="About Image"
+        />
         <div className="mt-4 md:mt-0 text-left flex-col h-full">
           <h2 className="text-4xl font-bold text-emerald-800">
             <br />
@@ -138,7 +188,7 @@ const AboutSection = () => {
             <br />
             About Me
           </h2>
-			<br/>
+          <br />
           <p className="text-base md:text-lg text-zinc-400">
             Hello{" "}
             <strong className="font-extrabold">I&apos;m Hemanth S Banur</strong>
@@ -151,9 +201,9 @@ const AboutSection = () => {
             </strong>
             <br />
             <br />
-            Of course, that&apos;s not all. I also happen to be a game developer and
-            a web developer. I mean, who needs a social life when you can spend
-            your days{" "}
+            Of course, that&apos;s not all. I also happen to be a game developer
+            and a web developer. I mean, who needs a social life when you can
+            spend your days{" "}
             <strong className="font-extrabold text-emerald-800">
               git push
             </strong>
@@ -169,7 +219,8 @@ const AboutSection = () => {
             <strong className="font-extrabold text-emerald-800">
               python3 ml_model.py
             </strong>{" "}
-            until the sun comes up. It&apos;s a tough job, but someone&apos;s gotta do it.
+            until the sun comes up. It&apos;s a tough job, but someone&apos;s
+            gotta do it.
             <br />
             <strong className="font-extrabold text-emerald-800">
               sudo apt-get install machine-learning
@@ -184,49 +235,46 @@ const AboutSection = () => {
             commands
             <br />
             <br />
-            Anyway, that&apos;s me in a nutshell. I&apos;m a well-rounded individual with
-            a diverse skillset and an insatiable thirst for knowledge. Now, if
-            you&apos;ll excuse me, I need to go{" "}
+            Anyway, that&apos;s me in a nutshell. I&apos;m a well-rounded
+            individual with a diverse skillset and an insatiable thirst for
+            knowledge. Now, if you&apos;ll excuse me, I need to go{" "}
             <strong className="font-extrabold text-emerald-800">
               sudo reboot
             </strong>{" "}
             my brain and come up with some more captivating hobbies
             <br />
           </p>
-		<div className="mt-8">
-		<h5 className="text-3xl font-bold text-emerald-800 mb-3">
-		My Tech Stack
-		</h5>
-          <div className="flex flex-row justify-start mr-12">
-            <TabButton
-              selectTab={() => handleTabChange("devtools")}
-              active={tab === "devtools"}
-			  className="mr-4"
-            >
-              {" "}
-				Dev Tools
-              {" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("webdev")}
-              active={tab === "webdev"}
-			  className="mr-4"
-            >
-              {" "}
-				Web Dev
-			  {" "}
-            </TabButton>
-			<TabButton
-			  selectTab={() => handleTabChange("langs")}
-			  active={tab === "langs"}
-			  className="mr-4"
-			>
-			  {" "}
-				Languages
-			  {" "}
-			</TabButton>
+          <div className="mt-8">
+            <h5 className="text-3xl font-bold text-emerald-800 mb-3">
+              My Tech Stack
+            </h5>
+            <div className="flex flex-row justify-start mr-12">
+              <TabButton
+                selectTab={() => handleTabChange("devtools")}
+                active={tab === "devtools"}
+                className="mr-4"
+              >
+                {" "}
+                Dev Tools{" "}
+              </TabButton>
+              <TabButton
+                selectTab={() => handleTabChange("webdev")}
+                active={tab === "webdev"}
+                className="mr-4"
+              >
+                {" "}
+                Web Dev{" "}
+              </TabButton>
+              <TabButton
+                selectTab={() => handleTabChange("langs")}
+                active={tab === "langs"}
+                className="mr-4"
+              >
+                {" "}
+                Languages{" "}
+              </TabButton>
+            </div>
           </div>
-		</div>
           <div className="mt-8 mr-12">
             {TAB_DATA.find((t) => t.id === tab).content}
           </div>
