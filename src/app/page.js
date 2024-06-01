84a6b0e65cb3e8878a7d90764b328b429d0c61ed
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-06-01 21:36:43 +0530

    font

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
+import './globals.css';
 
 export default function Home() {
   return (
