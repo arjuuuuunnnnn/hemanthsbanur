794435cede0379c8778f5ba4e288eb1cb818a143
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-06-15 12:27:28 +0530

    footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-top-emerald-800 border-l-transparent text-zinc-400">
+    <footer className="footer border z-5 border-top-emerald-800 border-l-transparent text-zinc-400">
       <div className="container p-12 flex justify-between">
         <Link href="/" className="hover:text-emerald-800 ">arjuuuuunnnnn</Link>
         <p className="">There&apos;s no place like <br/> <Link href="/" className="font-mono font-bold text-xl hover:text-emerald-800 ">&nbsp;&nbsp;127.0.0.1</Link></p>
