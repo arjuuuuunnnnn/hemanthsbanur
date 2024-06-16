7cee73a7a450c7b64249c3c8f19dadf8d31777ac
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-06-16 20:49:28 +0530

    footer ch

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
   return (
     <footer className="footer border z-5 border-top-emerald-800 border-l-transparent text-zinc-400">
-      <div className="container p-12 flex justify-between">
-        <Link href="/" className="hover:text-emerald-800 ">arjuuuuunnnnn</Link>
-        <p className="">There&apos;s no place like <br/> <Link href="/" className="font-mono font-bold text-xl hover:text-emerald-800 ">&nbsp;&nbsp;127.0.0.1</Link></p>
+      <div className="container p-12 flex justify-center">
+        <p>arjuuuuunnnnn @ <span className="hover:text-emerald-800">
+	  <Link href="/">
+	  127.0.0.1
+	  </Link>
+	  </span>
+	  <br/>
+	  <span className="flex justify-center space-between-2">
+	  <Link href="https://github.com/arjuuuuunnnnn/">
+	  <FaGithub className="hover:scale-105 hover:text-white mt-2 mr-4"/>
+	  </Link>
+	  <Link href="https://www.linkedin.com/in/hemanth-s-banur-3aaa34284/">
+	  <FaLinkedin className="hover:scale-105 hover:text-white mt-2 mr-4"/>
+	  </Link>
+	  <Link href="mailto:arjunbanur27@gmail.com">
+	  <SiGmail className="hover:scale-105 hover:text-white mt-2 mr-4"/>
+	  </Link>
+	  </span>
+	  </p>
       </div>
     </footer>
   );
