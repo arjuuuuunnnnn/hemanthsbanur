45562795c35c39615e4cbe941786097d6c509767
arjuuuuunnnnn <arjunbanur27@gmail.com>
2024-07-31 20:39:25 +0530

    jsonnn

--- src/app/cv/page.jsx
+++ src/app/cv/page.jsx
@@ -5,66 +5,16 @@ import { FaLinkedin, FaPhoneAlt, FaGithub } from "react-icons/fa";
 import { SlGlobe } from "react-icons/sl";
 import Image from "next/image";
 
-const projects = [
-  {
-    name: "Cropify",
-    description: "Cropify is a web application built using React.js, Flask & Machine learning models in the backend",
-    link: "https://github.com/arjuuuuunnnnn/Cropify",
-    tags: ["ReactJS", "Flask", "scikit-learn", "keras"]
-  },
-  {
-    name: "MLOps",
-    description: ["1. Credit Card Fraud Detection 2. Customer Satisfaction.", "Used pipelining, DagsHub, ZenML, mlFlow"],
-    link: "https://github.com/arjuuuuunnnnn/CreditCard-Fraud-Detect-MLOps",
-    tags: ["dagshub", "mlflow", "python"]
-  },
-  {
-    name: "Machine Learning",
-    description: "1. Wine quality 2. Kidney disease classification. Data Ingestion, Data Validation, Data Transformation, Model Training, Prediction Pipeline",
-    link: "https://github.com/arjuuuuunnnnn/Wine-Quality-End-to-End-ML",
-    tags: ["scikit-learn", "dvc", "python"]
-  },
-  {
-    name: "Reg-Model",
-    description: "Built a Regression model for a particular dataset with all the statistical analysis",
-    link: "https://github.com/arjuuuuunnnnn/Reg-model",
-    tags: ["Data Analysis", "scikit-learn", "python"]
-  },
-  {
-    name: "Zombie v/s player 3D",
-    description: "An Unity Developed 3D game",
-    link: "https://github.com/arjuuuuunnnnn/Zombie-3D",
-    tags: ["Unity", "C#"]
-  }
-];
-
-const skills = {
-  Languages: ["C/C++", "C#", "Python", "JavaScript", "TypeScript", "MATLAB", "HTML", "CSS"],
-  Libraries: ["Pandas", "Numpy", "Scikit-Learn", "OpenCV", "PyTorch", "TensorFlow", "Matplotlib", "Seaborn", "NLTK"],
-  "Frameworks/Tools": ["Docker", "React.js", "Node.js", "MongoDB", "Git", "Unity", "Azure", "mlflow", "ZenML", "Flask", "Dagshub", "Firebase", "Django", "Flutter", "Linux"]
-};
-
-const activitiesAndAchievements = [
-  {
-    title: "Practical Approach to ML:",
-    description: "Won 1st place in the hackathon organised by CIE PESU held for 8 weeks (Apr 2024)"
-  },
-  {
-    title: "Appex | Technical Lead",
-    description: "Organised \"AppGenesis,\" an ideation event, served as a mentor for the same, providing guidance and support to participants throughout the event"
-  },
-  {
-    title: "Aura | Technical member",
-    description: "Organized \"Crayion\", a hackathon event focused on image generation using advanced models such as DALL-E and Midjourney"
-  }
-];
-
+import projects from './projects.json';
+import skills from './skills.json';
+import activitiesAndAchievements from './act-ach.json';
+import experience from './experience.json';
 
 const ResumePage = () => {
   return (
     <div className="min-h-screen bg-[#000000] text-zinc-400 p-4 md:p-8">
-      <div className="max-w-4xl mx-auto">
-        <div className="flex justify-between items-start mb-8">
+      <div className="max-w-3xl mx-auto">
+        <div className="flex justify-between items-start mb-4">
           <div className="flex-1 pr-4">
             <h1 className="text-2xl font-bold text-zinc-300">Hemanth S Banur</h1>
             <p className="text-sm text-gray-400 mb-2">Machine Learning & MLOps Enthusiast</p>
@@ -87,29 +37,26 @@ const ResumePage = () => {
           </div>
         </div>
 
-		<section className="mb-8">
-          <h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-4 text-zinc-300">Summary</h2>
+        <section className="mb-4">
+          <h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-2 text-zinc-300">Summary</h2>
           <p className="text-sm">
             Passionate Machine Learning and MLOps enthusiast with a strong foundation in Computer Science. 
-            Experienced in developing ML models, implementing MLOps practices, and creating web applications. 
-            Skilled in various programming languages and frameworks, with a keen interest in solving complex 
-            problems through innovative technological solutions. Committed to continuous learning and 
-            staying updated with the latest advancements in software development.
+            Experienced in developing ML models, implementing MLOps practices, and creating web applications.
           </p>
         </section>
 		
-		<section className="mb-8">
-			<h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-4 text-zinc-300">Skills</h2>
-				{Object.entries(skills).map(([category, skillList]) => (
-			<div key={category} className="mb-2">
-				<h3 className="font-semibold text-sm text-zinc-300">{category}:</h3>
-				<p className="text-sm">{skillList.join(", ")}</p>
-			</div>
-			))}
-		</section>
+        <section className="mb-4">
+          <h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-2 text-zinc-300">Skills</h2>
+          {Object.entries(skills).map(([category, skillList]) => (
+            <div key={category} className="mb-2">
+              <h3 className="font-semibold text-sm text-zinc-300">{category}:</h3>
+              <p className="text-sm">{skillList.join(", ")}</p>
+            </div>
+          ))}
+        </section>
 	    
-        <section className="mb-8">
-          <h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-4 text-zinc-300">Education</h2>
+        <section className="mb-4">
+          <h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-2 text-zinc-300">Education</h2>
           <div>
             <div className="flex justify-between items-center">
               <h3 className="font-semibold text-base text-zinc-300">PES University</h3>
@@ -120,49 +67,20 @@ const ResumePage = () => {
           </div>
         </section>
 
-        <section className="mb-8">
-          <h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-4 text-zinc-300">Experience</h2>
-          {[
-            
-			{
-              title: "IEEE CS, PES University",
-              position: "Open Source Project Initiative",
-              duration: "Jul 2024 - Aug 2024, Part-time",
-              location: "Bangalore",
-              description: ["Developing a hybrid GAN-VAE model which converts a normal picture into pencil sketch"]
-            },
-			  {
-              title: "Toyota Kirloskar Motor",
-              position: "Data Science Intern",
-              duration: "Jun 2024 - Jul 2024, Full-time",
-              location: "Bangalore, India",
-              description: ["Developed an automated web scraping and review analysis system using NLTK, used by all the employees reducing manual work time from 6-7 hours to 8-10 seconds",
-				  "Developed an automated daily remote data pre-processor, analyser, visualizer"
-			  ]
-            },
-            {
-              title: "CDSAML, PES University",
-              position: "Research Intern",
-              duration: "Jun 2024 - Jul 2024, Part-time",
-              location: "Bangalore",
-              description: [
-                "Developed a multi-modal pipeline for converting Kannada speech to English Text",
-                "Stages involving Audio denoising, Enhancing, segmenting, Transcripting, Punctuating, Translating, Grammar checking",
-				"User can edit the output of each stage or let as it is, so that it's fed as input for successive stage"
-              ]
-            }
-		  ].map((exp, index) => (
+        <section className="mb-4">
+          <h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-2 text-zinc-300">Experience</h2>
+          {experience.map((exp, index) => (
             <div key={index} className="mb-4">
               <div className="flex justify-between items-center">
-			  <div>
-                <h3 className="font-semibold text-base text-zinc-300">{exp.title}</h3>
-			  <p className="text-sm text-zinc-300">{exp.position}</p>
-			  </div>
-			  <div className="text-right">
-				<span className="text-xs font-normal">{exp.duration}</span>
-			  <p className="text-xs">{exp.location}</p>
-				</div>
-                  </div>
+                <div>
+                  <h3 className="font-semibold text-base text-zinc-300">{exp.title}</h3>
+                  <p className="text-sm text-zinc-300">{exp.position}</p>
+                </div>
+                <div className="text-right">
+                  <span className="text-xs font-normal">{exp.duration}</span>
+                  <p className="text-xs">{exp.location}</p>
+                </div>
+              </div>
               {Array.isArray(exp.description) ? (
                 <ul className="list-disc ml-6 text-sm">
                   {exp.description.map((item, idx) => (
@@ -177,7 +95,7 @@ const ResumePage = () => {
         </section>
 
         <section>
-          <h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-4 text-zinc-300">Projects</h2>
+          <h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-2 text-zinc-300">Projects</h2>
           {projects.map((project, index) => (
             <div key={index} className="mb-4">
               <h3 className="font-semibold text-zinc-300">
@@ -193,15 +111,15 @@ const ResumePage = () => {
           ))}
         </section>
 
-	    <section className="mb-8">
-			<h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-4 text-zinc-300">Activities & Achievements</h2>
-			{activitiesAndAchievements.map((item, index) => (
-			<div key={index} className="mb-2">
-				<h3 className="font-semibold text-sm text-zinc-300">{item.title}</h3>
-				<p className="text-sm">{item.description}</p>
-			</div>
-			))}
-		</section>
+        <section className="mb-4">
+          <h2 className="text-lg font-semibold border-b border-emerald-800 pb-1 mb-2 text-zinc-300">Activities & Achievements</h2>
+          {activitiesAndAchievements.map((item, index) => (
+            <div key={index} className="mb-2">
+              <h3 className="font-semibold text-sm text-zinc-300">{item.title}</h3>
+              <p className="text-sm">{item.description}</p>
+            </div>
+          ))}
+        </section>
       </div>
     </div>
   );
